f7709fd64b0ef81053718a6e893d7c4f41043642
Asil Awad <aseelawad8@hotmail.com>
2025-08-01 18:35:10 +0300

    add student Constructor

--- bin/assignment5/student.dart
+++ bin/assignment5/student.dart
@@ -1,7 +1,11 @@
-class Student{
+class Student {
   String _name;
   double _grade;
   String? _email;
   String? _address;
   String? _phone;
+
+  Student({required String name, required double grade})
+      : _name = name,
+        _grade = grade;
 }
