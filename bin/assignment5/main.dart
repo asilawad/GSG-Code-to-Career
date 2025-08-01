fafbace14a5e6555b3e28d68acadf10c051001f1
Asil Awad <aseelawad8@hotmail.com>
2025-08-01 18:45:34 +0300

    set students optional properties

--- bin/assignment5/main.dart
+++ bin/assignment5/main.dart
@@ -6,4 +6,13 @@ void main() {
   students.add(Student(name: "Ali", grade: 85));
   students.add(Student(name: "Sara", grade: 92));
   students.add(Student(name: "Omar", grade: 55));
+
+  students[0].email = "ali@mail.com";
+  students[0].phone = "123456";
+
+  students[1].email = "sara@mail.com";
+  students[1].address = "Main Street";
+
+  students[2].phone = "789123";
+  students[2].address = "Garden Road";
 }
