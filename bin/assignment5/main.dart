0bf89c517ca430e0fd92d5fab5bea53df267036e
Asil Awad <aseelawad8@hotmail.com>
2025-08-01 18:43:35 +0300

    create strudents objects

---
+++ bin/assignment5/main.dart
@@ -0,0 +1,9 @@
+import 'student.dart';
+
+void main() {
+  List<Student> students = [];
+
+  students.add(Student(name: "Ali", grade: 85));
+  students.add(Student(name: "Sara", grade: 92));
+  students.add(Student(name: "Omar", grade: 55));
+}
